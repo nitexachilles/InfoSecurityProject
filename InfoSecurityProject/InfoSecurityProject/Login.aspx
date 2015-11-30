e58aae8c5a34303df06506e0a23de98c879371ed
Mohammad Shameel <133047B@mymail.nyp.edu.sg>
2015-11-30 10:11:39 +0800

    Added

---
+++ InfoSecurityProject/InfoSecurityProject/Login.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Registration.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="InfoSecurityProject.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    
+</asp:Content>
--- InfoSecurityProject/InfoSecurityProject/Registration.Master.designer.cs
+++ InfoSecurityProject/InfoSecurityProject/Registration.Master.designer.cs
@@ -38,14 +38,5 @@ namespace InfoSecurityProject {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-        
-        /// <summary>
-        /// BackgroundImage control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image BackgroundImage;
     }
 }
